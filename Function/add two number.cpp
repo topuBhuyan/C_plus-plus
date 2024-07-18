4987941c68286be4117a7e40789da48b00317d00
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-07-18 16:58:17 +0600

    Add files via upload

---
+++ Function/add two number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int add(int a, int b){
+    return (a+b);
+}
+
+int main(){
+    int sum;
+
+    sum =add(20,30);
+
+    cout << "20 + 30 = " << sum <<'\n';
+
+    return 0;
+}
+
+
