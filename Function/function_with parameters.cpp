a2f777317f2ac441ec473832edf6ade8e60e3f0b
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-07-18 12:16:16 +0600

    Add files via upload

---
+++ Function/function_with parameters.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// display a number
+void displayNum(int n, float n2){
+    cout <<"the number is int: " << n <<'\n';
+    cout <<"the number is float: " << n2 <<'\n';
+}
+
+
+int main(){
+    int num = 5;
+    float num2 = 10.5;
+
+    displayNum(num, num2);
+
+    return 0;
+}
+
