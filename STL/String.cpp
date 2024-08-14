e51c94c3caa96929d64404af1474130d60609f33
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-08-14 06:55:19 +0600

    Update String.cpp

--- STL/String.cpp
+++ STL/String.cpp
@@ -6,141 +6,148 @@ using namespace std;
 
 int main()
 {
-//    string s;
-//
-//    s = s + 'a';
-//    s = s + 'b';
-//    s += 'c';
-//    cout <<s  <<endl;
-//
+    string s;
 
-//    string s = "topubhuyan";
-//
-//    s[0] = 'x';
-//    cout <<s[0] <<" " <<s[1] <<" " <<s[2] <<" " <<s[3];
+    s = s + 'a';
+    s = s + 'b';
+    s += 'c';
+    cout <<s  <<endl;
+_____________________________________
 
+    string s = "topubhuyan";
+    
+    s[0] = 'x';
+    cout <<s[0] <<" " <<s[1] <<" " <<s[2] <<" " <<s[3];
 
-//    string s;
-//    string s1 = "Topu", s2 = "Bhuyan";
-//    char c = ' ';
-//
-//    //s1.clear();
-//    s = s1 + c + s2;
-//    cout << s <<endl;
-//    //cout <<s1.empty();
+_____________________________________
+
+    string s;
+    string s1 = "Topu", s2 = "Bhuyan";
+    char c = ' ';
+
+    //s1.clear();
+    s = s1 + c + s2;
+    cout << s <<endl;
+    //cout <<s1.empty();
+
+_____________________________________
 
     /// using iterator
 
-//    string s = "Topu bhuyan";
-//
-//    string ::iterator it;
-//
-//    for(it = s.begin(); it != s.end(); it++ ){
-//        cout << *it;
-//    }
-//    cout <<endl;
+    string s = "Topu bhuyan";
 
-//    /// using foreach loop
-//
-//    string s = "Topu bhuyan";
-//
-//    for(auto u : s) cout <<u;
-//        cout <<endl;
+    string ::iterator it;
 
-//        /// Condition
-//      string s1 = "Topu", s2 = "Bhuyan";
-//
-//      if(s1 == s2)
-//         cout <<"Equals" <<endl;
-//      else
-//        cout <<"Not Equals! " <<endl;
+    for(it = s.begin(); it != s.end(); it++ ){
+        cout << *it;
+    }
+    cout <<endl;
+_____________________________________
+    
+    /// using foreach loop
 
+    string s = "Topu bhuyan";
+
+    for(auto u : s) cout <<u;
+        cout <<endl;
+_____________________________________
+
+    /// Condition
+      string s1 = "Topu", s2 = "Bhuyan";
+
+      if(s1 == s2)
+         cout <<"Equals" <<endl;
+      else
+        cout <<"Not Equals! " <<endl;
+_____________________________________
+    
     /// Revers
 
-//        string s1 = "Topu", s2 = "Bhuyan";
-//
-//        reverse( s1.begin(), s1.end() );
-//        cout << s1 <<endl;
+        string s1 = "Topu", s2 = "Bhuyan";
 
+        reverse( s1.begin(), s1.end() );
+        cout << s1 <<endl;
+_____________________________________
+    
     /// plaindrome
 
-//        string s = "aabaa";
-//        string tem = s;
-//
-//        reverse( tem.begin(), tem.end() );
-//
-//        if(tem == s) cout <<"Plindrome\n";
-//        else cout <<"Not Palindrome\n";
+        string s = "aabaa";
+        string tem = s;
 
+        reverse( tem.begin(), tem.end() );
+
+        if(tem == s) cout <<"Plindrome\n";
+        else cout <<"Not Palindrome\n";
+_____________________________________
 
     /// cin and cout k fast kora -> #define optimize() ios_base::sync_with_stido(0); cin.tie(0); cout tie(0);
 
-//    string s;
-//    cin >>s;
-//
-//    cout <<s <<endl;
+    string s;
+    cin >>s;
 
-//    string s;
-//    cin >> s;   /// spase ar por baki man gula nite parbe na
-//    cout <<s <<endl;
+    cout <<s <<endl;
 
-//    string s;
-//
-//    getline(cin , s); /// space ar por bki sob man nite parebe
-//    cout << s <<endl;
+    string s;
+    cin >> s;   /// spase ar por baki man gula nite parbe na
+    cout <<s <<endl;
+_____________________________________
+    string s;
 
-//    int t;
-//    cin >> t;
-//
-//    for(int tc = 1; tc <= t; tc++){
-//        string s;
-//        char c;
-//        cin >> c;
-//
-//        getline(cin, s);
-//
-//        s = c + s;
-//
-//        cout <<"Case " <<tc <<" " <<s <<endl;
-//    }
+    getline(cin , s); /// space ar por bki sob man nite parebe
+    cout << s <<endl;
 
+    int t;
+    cin >> t;
+
+    for(int tc = 1; tc <= t; tc++){
+        string s;
+        char c;
+        cin >> c;
+
+        getline(cin, s);
+
+        s = c + s;
+
+        cout <<"Case " <<tc <<" " <<s <<endl;
+    }
+_____________________________________
     /// sorting
 
-//    string s = "dacbA";
-//    sort( s.begin(), s.end() );
-//    cout <<s <<endl;
-
+    string s = "dacbA";
+    sort( s.begin(), s.end() );
+    cout <<s <<endl;
+_____________________________________
     /// revers sorting
 
 //    string s = "dabA";
 //    sort( s.rbegin(), s.rend() );
 //    cout <<s <<endl;
-
+_____________________________________
     /// unique
 
-//    string s = "adcdeAAA";
-//
-//    sort(s.begin(), s.end());
-//
-//    int sz = unique (s.begin(), s.end()) - s.begin();
-//    for(int i=0; i<sz; i++ )
-//        cout <<s[i];
-//    cout <<endl;
+    string s = "adcdeAAA";
 
+    sort(s.begin(), s.end());
+
+    int sz = unique (s.begin(), s.end()) - s.begin();
+    for(int i=0; i<sz; i++ )
+        cout <<s[i];
+    cout <<endl;
+_____________________________________
     /// max and min
-//
-//    string s = "adcdeAA";
-//
-//    cout << *max_element( s.begin(), s.end() ) <<endl;
-//    cout << *min_element( s.begin(), s.end() ) <<endl;
 
+    string s = "adcdeAA";
+
+    cout << *max_element( s.begin(), s.end() ) <<endl;
+    cout << *min_element( s.begin(), s.end() ) <<endl;
+_____________________________________
     /// erase function
 
-//    string s = "adcdeAA";
-//
-//    s.erase( s.begin()+2 );
-//    cout << s <<endl;
+    string s = "adcdeAA";
 
+    s.erase( s.begin()+2 );
+    cout << s <<endl;
+_____________________________________
     ///  luxorographics
 
     vector <string> v;
