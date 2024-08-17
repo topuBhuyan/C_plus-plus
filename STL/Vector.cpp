559ace4b9fa728180c8725c7737b8a2eff831290
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-08-17 19:59:01 +0600

    Update Vector.cpp

--- STL/Vector.cpp
+++ STL/Vector.cpp
@@ -13,175 +13,186 @@ int main()
     }
     */
 
+/*
+    vector<int> v;
 
-//    vector<int> v;
-//
-//    v.push_back(2);
-//    v.push_back(4);
-//    v.push_back(5);
-//    v.push_back(7);
-//    v.push_back(9);
-//
-//    v[2] = 12; /// value resize
-//
-//    cout <<v[2];
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(7);
+    v.push_back(9);
+
+    v[2] = 12; /// value resize
+    cout <<v[2];
+*/
+    
+/*
+   vector<int> v;
+
+    v.push_back(2);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(7);
+    v.push_back(9);
+
+    cout <<v.size(); /// element size
+*/
+    
+    vector<int> v;
+
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(5);
+    v.push_back(7);
+
+    for(int i=0; i<v.size(); i++)
+    {
+        cout <<v[i] <<" ";
+    }
+*/
+
+    vector<int> v;
+
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(5);
+    v.push_back(7);
+
+    for(int i=0; i<4; i++)
+    {
+        cout <<v[i] <<" ";
+    }
 
 
-//   vector<int> v;
-//
-//    v.push_back(2);
-//    v.push_back(4);
-//    v.push_back(5);
-//    v.push_back(7);
-//    v.push_back(9);
-//
-//    cout <<v.size(); /// element size
+    vector<int> v={22,3,4,5};
 
-//
-//    vector<int> v;
-//
-//    v.push_back(2);
-//    v.push_back(3);
-//    v.push_back(5);
-//    v.push_back(7);
-//
-//    for(int i=0; i<v.size(); i++)
-//    {
-//        cout <<v[i] <<" ";
-//    }
-
-
-//    vector<int> v;
-//
-//    v.push_back(2);
-//    v.push_back(3);
-//    v.push_back(5);
-//    v.push_back(7);
-//
-//    for(int i=0; i<4; i++)
-//    {
-//        cout <<v[i] <<" ";
-//    }
-
-//
-//    vector<int> v={22,3,4,5};
-//
-//    for(int i=0; i<v.size(); i++)
-//    {
-//        cout <<v[i] <<" ";
-//    }
-
+    for(int i=0; i<v.size(); i++)
+    {
+        cout <<v[i] <<" ";
+    }
+*/
 
     /// Take input from User !!
+    int n;
+    vector<int> v;
 
-//    int n;
-//    vector<int> v;
-//
-//    cin >>n;
-//    for(int i=0; i<n; i++){
-//        int a;
-//        cin>>a;
-//        v.push_back(a);
-//    }
-//    cout <<v.size() <<endl;
-//
-//    for(int i=0; i<v.size(); i++){
-//        cout <<v[i] <<" ";
-//    }
-//    cout <<endl;
+    cin >>n;
+    for(int i=0; i<n; i++){
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+     cout <<v.size() <<endl;
 
-//
-//    vector<int> v={2,5,6,7,8};
-//
-//    cout<<v.size()<<endl;
-//
-//    for(int i=0; i<v.size(); i++)
-//    {
-//        cout <<v[i] <<" ";
-//    }
-//    cout <<endl;
+    for(int i=0; i<v.size(); i++){
+        cout <<v[i] <<" ";
+    }
+    cout <<endl;
+*/
+
+/*
+    
+    vector<int> v={2,5,6,7,8};
+
+    cout<<v.size()<<endl;
+
+    for(int i=0; i<v.size(); i++)
+    {
+        cout <<v[i] <<" ";
+    }
+    cout <<endl;
 
     /// v.clear()
 
-//    vector<int> v={4, 5, 7, 8, 3};
-//    v.clear();
-//
-//    cout <<v.size() <<endl;
+    vector<int> v={4, 5, 7, 8, 3};
+    v.clear();
 
+    cout <<v.size() <<endl;
+*/
+    
+/*
     /// Empty()
 
-//    vector<int> v ={4, 5, 8, 9};
-//
-//    v.clear();
-//    v = {2,3};
-//
-//    cout <<v.empty() <<endl;
-//
+    vector<int> v ={4, 5, 8, 9};
 
+    v.clear();
+    v = {2,3};
+
+    cout <<v.empty() <<endl;
+*/
+
+/*
     /// Resize
 
-//    vector<int> v={5, 4, 3, 7, 9};
-//    v.resize(10);
-//
-//    cout <<v.size() <<endl;
-//
-//    for(int i=0; i<v.size(); i++){
-//        cout <<v[i] <<" ";
-//    }
-//    cout <<endl;
+    vector<int> v={5, 4, 3, 7, 9};
+    v.resize(10);
 
-//     vector<int> v(10);
-//
-//     int n;
-//     cin >>n;
-//
-//     for(int i=0; i<n; i++){
-//        cin >>v[i];
-//     }
-//     cout <<v.size() <<endl;
-//
-//     for(int i=0; i<v.size(); i++){
-//        cout <<v[i] <<" ";
-//     }
-//     cout <<endl;
+    cout <<v.size() <<endl;
 
-//        vector<int> v(10, 5);
-//
-//        for(int i=0; i<v.size(); i++){
-//            cout <<v[i] <<" ";
-//        }
-//        cout <<endl;
+    for(int i=0; i<v.size(); i++){
+        cout <<v[i] <<" ";
+    }
+    cout <<endl;
+*/
 
+/*
+     vector<int> v(10);
+
+     int n;
+     cin >>n;
+
+     for(int i=0; i<n; i++){
+        cin >>v[i];
+     }
+     cout <<v.size() <<endl;
+
+     for(int i=0; i<v.size(); i++){
+        cout <<v[i] <<" ";
+     }
+     cout <<endl;
+*/
+/*
+        vector<int> v(10, 5);
+
+        for(int i=0; i<v.size(); i++){
+            cout <<v[i] <<" ";
+        }
+        cout <<endl;
+*/
+
+/*
     /// value Copy
 
-//    vector<int> v={2,4,6,7,8,9};
-//    vector<int> tem;
-//
-//    tem = v;
-//
-//    cout <<tem.size() <<endl;
-//    for(int i=0; i<tem.size(); i++){
-//        cout <<tem[i] <<" ";
-//    }
-//    cout <<endl;
-//
-//    cout <<v.size()<<endl;
-//    for(int i=0; i<v.size(); i++){
-//        cout <<v[i] <<" ";
-//    }
-//    cout <<endl;
+    vector<int> v={2,4,6,7,8,9};
+    vector<int> tem;
 
+    tem = v;
+
+    cout <<tem.size() <<endl;
+    for(int i=0; i<tem.size(); i++){
+        cout <<tem[i] <<" ";
+    }
+    cout <<endl;
+
+    cout <<v.size()<<endl;
+    for(int i=0; i<v.size(); i++){
+        cout <<v[i] <<" ";
+    }
+    cout <<endl;
+*/
+    
+/*
     /// iterator
 
-//    vector<int> v={2, 3, 4, 5};
-//
-//    vector<int>::iterator it;
-//
-//    for( it = v.begin(); it != v.end(); it++){
-//        cout << *it <<" ";
-//    }
-//    cout <<endl;
+    vector<int> v={2, 3, 4, 5};
 
+    vector<int>::iterator it;
+
+    for( it = v.begin(); it != v.end(); it++){
+        cout << *it <<" ";
+    }
+    cout <<endl;
+*/
     /// For each Loop
 //
 //    vector<int> v={2, 3, 4, 5};
