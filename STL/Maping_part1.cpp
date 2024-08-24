aab27ad312f2db92bfd60dde067d2a228231baef
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-08-24 20:21:41 +0600

    Update Maping_part1.cpp

--- STL/Maping_part1.cpp
+++ STL/Maping_part1.cpp
@@ -4,66 +4,71 @@ using namespace std;
 
 int main(){
 
-//    vector<int> v = {2, 3, 6, 8, 9};
-//
-//    for (int i = 0; i < 5; i++) {
-//            cont[v[i]]++;
-//    }
-    /// usign string and integer.
-//
-//    map<string , int > id;
-//
-//    id["topu"] = 1;
-//    id["rohim"] = 2;
-//    id["korim"] = 3;
-//
-//    cout << id["korim"] <<'\n';
+    vector<int> v = {2, 3, 6, 8, 9};
 
-    /// using string
+    for (int i = 0; i < 5; i++) {
+            cont[v[i]]++;
+    }
+_______________________________________
+    // usign string and integer.
 
-//    map < string , string > gender;
-//
-//    gender["topu"] = "Male";
-//    gender["akhi"] = "Female";
-//
-//    cout <<gender["topu"] << ' ' << gender["akhi"] <<'\n';
+    map<string , int > id;
 
-//    vector < long long > v = {1, 294734324, 21342142, 23423432};
-//
-//    map <long long, int> cnt;
-//
-//    for (int i=0; i<v.size(); i++) {
-//        cnt[v[i]]++;
-//    }
-//    cout << cnt[294734324] <<'\n';
-//
-    /// using for each
+    id["topu"] = 1;
+    id["rohim"] = 2;
+    id["korim"] = 3;
 
-//  vector < long long > v = {1, 294734324, 21342142, 23423432};
-//
-//    map <long long, int> cnt;
-//
-//    for( auto u : v) cnt[u]++;
-//
-//    for(auto u : cnt){
-//        cout << u.first <<" " << u.second <<'\n';
-//    }
+    cout << id["korim"] <<'\n';
+_______________________________________
 
-    /// string and value
+    // using string
 
-//    map <string, int> id;
-//
-//    id["topu"] = 3;
-//    id["rudra"] = 8;
-//    id["alif"] = 6;
-//    id["rohim"] = 2;
-//    id["korim"] = 5;
-//
-//    for(auto u : id) {
-//        cout << u.first <<' ' << u.second <<'\n';
-//    }
+    map < string , string > gender;
 
-    /// using bool
+    gender["topu"] = "Male";
+    gender["akhi"] = "Female";
+
+    cout <<gender["topu"] << ' ' << gender["akhi"] <<'\n';
+
+    vector < long long > v = {1, 294734324, 21342142, 23423432};
+
+    map <long long, int> cnt;
+
+    for (int i=0; i<v.size(); i++) {
+        cnt[v[i]]++;
+    }
+    cout << cnt[294734324] <<'\n';
+_______________________________________
+
+    // using for each
+
+  vector < long long > v = {1, 294734324, 21342142, 23423432};
+
+    map <long long, int> cnt;
+
+    for( auto u : v) cnt[u]++;
+
+    for(auto u : cnt){
+        cout << u.first <<" " << u.second <<'\n';
+    }
+_______________________________________
+
+    // string and value
+
+    map <string, int> id;
+
+    id["topu"] = 3;
+    id["rudra"] = 8;
+    id["alif"] = 6;
+    id["rohim"] = 2;
+    id["korim"] = 5;
+
+    for(auto u : id) {
+        cout << u.first <<' ' << u.second <<'\n';
+    }
+_______________________________________
+
+    // using bool
 
     map<int, bool> vis;
 
@@ -73,9 +78,6 @@ int main(){
 
     for(auto u : vis)
         cout << u.first << ' '  << u.second << '\n';
-
-
-
 
     return 0;
 }
