80e940f754239b49c4598d1b00f10407e535f7be
Topu <113087685+topuBhuyan@users.noreply.github.com>
2024-10-02 21:56:39 +0600

    Add files via upload

---
+++ Expression/expression.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//    int x, y, z; /// variable declaration
+//
+//    cout << "Enter the values of X and Y: ";
+//    cin >> x >> y;
+//
+//    z = x + y;
+//
+//    cout <<'\n' << "Value of z is: " <<z; /// displaying the value  of z
+
+//                &x
+//                ptr
+//                ptr++
+//                ptr-
+
+//    int a[] = {1,2,3,4,5}; /// array initialization
+//    int *ptr; /// pointer declaration
+//
+//    ptr = a;
+//    ptr = ptr+1;
+//
+//    cout << *ptr <<'\n';
+
+
+//    int a=2, b=7, c=4;
+//    cout <<( (a>b) || (a>c) );
+
+//    int x=5;
+//    cout << (x>>1) <<'\n';
+
+//    int a, b;
+//    a=b=80;
+//
+//    cout << a << " " <<b <<'\n';
+
+//      int a, b;
+//      a=10+(b=90);
+//
+//      cout << a <<'\n';
+
+        int a=30;
+        a += 10;
+        cout << a <<'\n';
+
+
+    return 0;
+}
+
+
