7b308796ca2dcaec2dc93ffcfb2e9acb55193526
Topu <113087685+topuBhuyan@users.noreply.github.com>
2024-10-06 08:41:43 +0600

    Update 4_find are of triangle.cpp

--- HSC_Book_Solved_convertin_C++/4_find are of triangle.cpp
+++ HSC_Book_Solved_convertin_C++/4_find are of triangle.cpp
@@ -14,20 +14,5 @@ int main()
 
     return 0;
 }
-#include<bits/stdc++.h>
-using namespace std;
 
-int main()
-{
-    int a, b, c;
-    cin >> a>> b>> c;
-
-    int s = (a+b+c);
-
-    int area = sqrt(s *(s-a)*(s-b)*(s-c) );
-
-    cout << area <<'\n';
-
-    return 0;
-}
 
