d758acb1313b2b26d78028345b38c9b5c9cd60e3
Topu <113087685+topuBhuyan@users.noreply.github.com>
2024-10-06 08:30:16 +0600

    Update 10_prime Number.cpp

--- HSC_Book_Solved_convertin_C++/10_prime Number.cpp
+++ HSC_Book_Solved_convertin_C++/10_prime Number.cpp
@@ -3,22 +3,23 @@ using namespace std;
 
 int main()
 {
-   int n, i, m=0, flag = 0;
-   cout <<"Enter the number of check prime: ";
-   cin >> n;
+    int n, i, m = 0, flag = 0;
+    cout << "Enter the number: ";
+    cin >> n;
 
-   m = n/2;
+    m = n/2;
 
-   for(i = 2; i<= m; i++) {
-    if(n % i == 0) {
+    for(i = 2; i<=m; i++){
 
-        cout <<"Number is not prime" <<'\n';
-        flag = 1;
-        break;
+        if(n % i == 0){
+            cout << "Number not prime: \n" ;
+            flag = 1;
+            break;
+        }
     }
-   }
-   if(flag == 0)
-    cout <<"Number is Prime" <<'\n';
+    if(flag == 0)
+        cout << "Number is Prime:  \n";
 
     return 0;
 }
+
